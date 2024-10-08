ae67e61ecd1ce2c898d5ceb1b438c8081dbc4056
EnderKill98 <no@mail.tld>
2024-10-09 01:07:53 +0200

    Update to 1.21 and bump version to 0.2.1

--- src/client/java/me/enderkill98/mixin/client/mods/patpat/PatPatClientPacketManagerInvoker.java
+++ src/client/java/me/enderkill98/mixin/client/mods/patpat/PatPatClientPacketManagerInvoker.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.UUID;
 
-@Mixin(PatPatClientPacketManager.class)
+@Mixin(value = PatPatClientPacketManager.class, remap = false)
 public interface PatPatClientPacketManagerInvoker {
 
     @Invoker("handlePatting")
